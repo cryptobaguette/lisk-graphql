5daf865ccdbb0af3e760d870bdccb8d153402261
cryptobaguette <cryptobaguette@tutanota.com>
2018-06-28 23:26:52 +0200

    test find account with publicKey

--- __tests__/graphql/queries/account.js
+++ __tests__/graphql/queries/account.js
@@ -1,11 +1,32 @@
 const { graphql } = require('graphql');
 const { schema } = require('../../../src/graphql');
 
+const address = '16009998050678037905L';
+const publicKey =
+  '73ec4adbd8f99f0d46794aeda3c3d86b245bd9d27be2b282cdd38ad21988556b';
+
 describe('account', () => {
-  it('should fetch account', async () => {
+  it('should fetch account with address', async () => {
     const query = `
       query account {
-        account(address: "16009998050678037905L") {
+        account(address: "${address}") {
+          address
+          balance
+          unconfirmedBalance
+          publicKey
+          secondPublicKey
+        }
+      }
+    `;
+    const { data, errors } = await graphql(schema, query, {}, {});
+    expect(errors).toBeUndefined();
+    expect(data.account.address).toBeTruthy();
+  });
+
+  it('should fetch account with publicKey', async () => {
+    const query = `
+      query account {
+        account(publicKey: "${publicKey}") {
           address
           balance
           unconfirmedBalance
