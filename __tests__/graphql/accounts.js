49c6848fcc4d6731102001d9ca6d40133886db35
cryptobaguette <cryptobaguette@tutanota.com>
2018-06-28 22:36:07 +0200

    add limit test

--- __tests__/graphql/accounts.js
+++ __tests__/graphql/accounts.js
@@ -12,6 +12,23 @@ describe('graphql accounts', () => {
     `;
     const { data, errors } = await graphql(schema, query, {}, {});
     expect(errors).toBeUndefined();
-    expect(data).toEqual({});
+    expect(data.accounts.length).toBe(100);
+  });
+
+  describe('parameters', () => {
+    it('should limit', async () => {
+      const query = `
+        query accounts {
+          accounts(limit: 10) {
+            address
+          }
+        }
+      `;
+      const { data, errors } = await graphql(schema, query, {}, {});
+      expect(errors).toBeUndefined();
+      expect(data.accounts.length).toBe(10);
+    });
+
+    // TODO test offset
   });
 });
