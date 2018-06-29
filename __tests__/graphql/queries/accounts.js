05dd9f4563d577a12e6b04d4f4bbfeece501c417
cryptobaguette <cryptobaguette@tutanota.com>
2018-06-29 16:01:04 +0200

    test offset

--- __tests__/graphql/queries/accounts.js
+++ __tests__/graphql/queries/accounts.js
@@ -47,15 +47,37 @@ describe('accounts', () => {
         expect(data.accounts.length).toBe(50);
         expect(data.accounts).toEqual(apiResponse.data.map(apiFormatAccount));
       });
+    });
 
-      // TODO test offset
+    describe('offset', () => {
+      it('should offset', async () => {
+        const apiResponse = await testnetClient.accounts.get({
+          offset: 10,
+        });
+        const query = `
+          query accounts {
+            accounts(offset: 10) {
+              address
+              balance
+              unconfirmedBalance
+              publicKey
+              secondPublicKey
+            }
+          }
+        `;
+        const { data, errors } = await graphql(schema, query, {}, {});
+        expect(errors).toBeUndefined();
+        expect(data.accounts.length).toBe(50);
+        expect(data.accounts).toEqual(apiResponse.data.map(apiFormatAccount));
+      });
+    });
 
-      describe('sort', () => {
-        it('should sort by BALANCE_DESC', async () => {
-          const apiResponse = await testnetClient.accounts.get({
-            sort: 'balance:desc',
-          });
-          const query = `
+    describe('sort', () => {
+      it('should sort by BALANCE_DESC', async () => {
+        const apiResponse = await testnetClient.accounts.get({
+          sort: 'balance:desc',
+        });
+        const query = `
             query accounts {
               accounts(sort: BALANCE_DESC) {
                 address
@@ -66,11 +88,10 @@ describe('accounts', () => {
               }
             }
           `;
-          const { data, errors } = await graphql(schema, query, {}, {});
-          expect(errors).toBeUndefined();
-          expect(data.accounts.length).toBe(10);
-          expect(data.accounts).toEqual(apiResponse.data.map(apiFormatAccount));
-        });
+        const { data, errors } = await graphql(schema, query, {}, {});
+        expect(errors).toBeUndefined();
+        expect(data.accounts.length).toBe(10);
+        expect(data.accounts).toEqual(apiResponse.data.map(apiFormatAccount));
       });
     });
   });
