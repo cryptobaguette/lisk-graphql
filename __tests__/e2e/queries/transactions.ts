51614b26e6b1c701fc04cf661f8010c0a523fc38
cryptobaguette <cryptobaguette@tutanota.com>
2018-11-11 14:55:00 +0100

    test workings

--- __tests__/e2e/queries/transactions.ts
+++ __tests__/e2e/queries/transactions.ts
@@ -23,8 +23,25 @@ describe('transactions', () => {
         }
       }
     `;
-    const data = await graphqlClient.request<{ transactions: any }>(query);
-    expect(apiResponse.data[0]).toEqual(data.transactions[0]);
+    const data = await graphqlClient.request<{ transactions: any[] }>(query);
+    expect(apiResponse.data[0].id).toEqual(data.transactions[0].id);
+    expect(apiResponse.data[0].height).toEqual(data.transactions[0].height);
+    expect(apiResponse.data[0].blockId).toEqual(data.transactions[0].blockId);
+    expect(apiResponse.data[0].type).toEqual(data.transactions[0].type);
+    expect(apiResponse.data[0].timestamp.toString()).toEqual(
+      data.transactions[0].timestamp
+    );
+    expect(apiResponse.data[0].senderId).toEqual(data.transactions[0].senderId);
+    expect(
+      apiResponse.data[0].recipientId === ''
+        ? null
+        : apiResponse.data[0].recipientId
+    ).toEqual(data.transactions[0].recipientId);
+    expect(apiResponse.data[0].amount).toEqual(data.transactions[0].amount);
+    expect(apiResponse.data[0].fee).toEqual(data.transactions[0].fee);
+    expect(apiResponse.data[0].confirmations).toEqual(
+      data.transactions[0].confirmations
+    );
   });
 
   it('should fetch 10 transactions', async () => {
