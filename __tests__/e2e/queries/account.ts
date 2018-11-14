88d8982dac70b769c46fffce2c67818c6a4791a7
cryptobaguette <cryptobaguette@tutanota.com>
2018-11-14 15:04:07 +0100

    test with publicKey

--- __tests__/e2e/queries/account.ts
+++ __tests__/e2e/queries/account.ts
@@ -1,18 +1,34 @@
 import { graphqlClient } from '../../testUtils';
 
 const address = '6238004142195673234L';
+const publicKey =
+  'd85d2106558fa3946c9b67fa7a8984f1b8d6e58e9ca559055555f5aee6aa280c';
 
 describe('account', () => {
   describe('args', () => {
     describe('address', () => {
       it('should fetch account with address', async () => {
         const query = `
-            query account {
-              account(address: "${address}") {
-                address
-              }
+          query account {
+            account(address: "${address}") {
+              address
             }
-          `;
+          }
+        `;
+        const data = await graphqlClient.request<{ account: any }>(query);
+        expect(data.account.address).toBeTruthy();
+      });
+    });
+
+    describe('publicKey', () => {
+      it('should fetch account with publicKey', async () => {
+        const query = `
+          query account {
+            account(publicKey: "${publicKey}") {
+              address
+            }
+          }
+        `;
         const data = await graphqlClient.request<{ account: any }>(query);
         expect(data.account.address).toBeTruthy();
       });
