d32a05e15ef4dbe349ffbc599ead826535131608
cryptobaguette <cryptobaguette@tutanota.com>
2019-05-12 17:54:45 +0200

    Update

--- src/helpers/validators.ts
+++ src/helpers/validators.ts
@@ -1,4 +1,4 @@
-import validator from 'validator';
+import { isHexadecimal } from 'validator';
 
 export const isPublicKeyValid = (publicKey: string) =>
-  publicKey.length === 64 && validator.isHexadecimal(publicKey);
+  publicKey.length === 64 && isHexadecimal(publicKey);
