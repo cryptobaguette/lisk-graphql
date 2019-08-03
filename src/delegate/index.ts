7bfb0a03410a6cccfc09c129f7e4ad27a0fd0440
cryptobaguette <cryptobaguette@tutanota.com>
2019-08-03 23:03:46 +0200

    Remove old code

--- src/delegate/index.ts
+++
@@ -1,6 +0,0 @@
-// https://github.com/LiskHQ/lisk/blob/0.9.15/logic/delegate.js
-
-export { typeDef } from './typeDef';
-export { monster } from './monster';
-export { Query } from './query';
-export { resolver } from './resolver';
--- src/transaction/resolver.ts
+++
@@ -1,6 +0,0 @@
-export const resolver = {
-  Transaction: {
-    timestampRaw: (row: any) => row.timestamp,
-    amountRaw: (row: any) => row.amount,
-  },
-};
